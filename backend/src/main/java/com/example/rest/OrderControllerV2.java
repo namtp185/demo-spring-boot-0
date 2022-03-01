18016e402abf3b49cc04ff5bde05506a5f38a22e
namtp185 <namtp185@gmail.com>
2022-03-01 22:38:08 +0700

    route: update order api v2

--- backend/src/main/java/com/example/rest/OrderControllerV2.java
+++ backend/src/main/java/com/example/rest/OrderControllerV2.java
@@ -3,6 +3,10 @@ package com.example.rest;
 import com.example.rest.request.OrderRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @Slf4j
 @RestController
@@ -11,12 +15,27 @@ public class OrderControllerV2 {
 
     @PostMapping()
     public void createOrder(@RequestBody OrderRequest orderRequest) {
-        log.info("orderRequest: {}", orderRequest);
+        log.info("post with orderRequest: {}", orderRequest);
     }
 
     @GetMapping(value = "/{id}")
     public void getOrderDetail(@PathVariable(name = "id") int id) {
-        log.info("orderId: {}", id);
+        log.info("get with orderId: {}", id);
+    }
+
+    @GetMapping()
+    public void searchOrder(@RequestParam(name = "keyword") String keyword) {
+        log.info("get with order param: keyword: {}", keyword);
+    }
+
+    @PutMapping(value="/{id}")
+    public void editOrder(@PathVariable(name = "id") int id, @RequestBody OrderRequest orderRequest) {
+        log.info("put with orderRequest: {}", orderRequest);
+    }
+
+    @DeleteMapping(value="/{id}")
+    public void deleteOrder(@PathVariable(name = "id") int id) {
+        log.info("delete with orderId: {}", id);
     }
 }
 
